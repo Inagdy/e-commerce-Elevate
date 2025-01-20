453ff87f5daa2edd5cc0a690e2ff61ad5aec9846
ahmedkhaled <xahmedkhaledxzx@gmail.com>
2025-01-20 13:08:26 +0200

    unite testing categories

---
+++ src/app/mocapdata/adaptcategoreis/adaptcategories.ts
@@ -0,0 +1,21 @@
+export const categoriesadapt=[
+    {
+      "_id": "673c46fd1159920171827c85",
+      "name": "flowers",
+      "slug": "flowers",
+      "image": "https://flower.elevateegy.com/uploads/39c641a6-4ec4-421a-8f55-5d8f5eeba5c3-flowers.png",
+      "createdAt": "2024-11-19T08:06:21.263Z",
+      "updatedAt": "2024-11-19T08:06:21.263Z",
+      "productsCount": 9
+    },
+    {
+      "_id": "673c472f1159920171827c8a",
+      "name": "gifts",
+      "slug": "gifts",
+      "image": "https://flower.elevateegy.com/uploads/79af9251-8534-4d50-8346-160f30589268-gift.png",
+      "createdAt": "2024-11-19T08:07:11.634Z",
+      "updatedAt": "2024-11-19T08:07:11.634Z",
+      "productsCount": 0
+    },
+    // ... other categories
+  ]
