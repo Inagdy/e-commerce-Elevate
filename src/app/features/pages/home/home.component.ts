df8450b8e5c9b3b9f994105627a682f840f2e9f0
Inagdy <islam.m.nagdy@gmail.com>
2025-01-27 06:52:39 +0200

    fixed:issue with navbar solving commint

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../navbar/navbar.component";
-import { PoularIteamsComponent } from "../poular-iteams/poular-iteams.component";
+import { NavbarComponent } from "../../../core/pages/navbar/navbar.component";
+import { PoularIteamsComponent } from "../../../core/pages/poular-iteams/poular-iteams.component";
 
 
 @Component({
