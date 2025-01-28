00e52daf62b9bb9da08c4c3ecb14ae57bd3b40b8
Inagdy <islam.m.nagdy@gmail.com>
2025-01-28 13:43:38 +0200

    chore(About-us):build about us component still not completed responsive issue

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../core/pages/navbar/navbar.component";
 import { PoularIteamsComponent } from "../../../core/pages/poular-iteams/poular-iteams.component";
 import { GategoriesSliderComponent } from "../gategories-slider/gategories-slider.component";
+import { AboutUsComponent } from "../about-us/about-us.component";
 
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, PoularIteamsComponent, GategoriesSliderComponent],
+  imports: [NavbarComponent, PoularIteamsComponent, GategoriesSliderComponent, AboutUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
