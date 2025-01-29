631cc7bae0bd2b5e1735130b55d010a11e509708
ahmedkhaled <xahmedkhaledxzx@gmail.com>
2025-01-30 01:20:42 +0200

    add ourgallaryComonet and add responseve in pouplar-items componet

--- src/app/features/componet/home/home.component.ts
+++ src/app/features/componet/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { PoularIteamsComponent } from "./componet/poular-iteams/poular-iteams.component";
+import { OurgallaryComponent } from "./componet/ourgallary/ourgallary.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ PoularIteamsComponent],
+  imports: [PoularIteamsComponent, OurgallaryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
