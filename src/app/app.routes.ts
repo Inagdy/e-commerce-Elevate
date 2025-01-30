e8aea8cb1529849777fb378d77202d4c8c6ab9c3
Inagdy <islam.m.nagdy@gmail.com>
2025-01-31 00:16:43 +0200

    update master after merging featiure and solveing some conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path:"" ,redirectTo:"home",pathMatch:"full"},
 
-    {path:"home", loadComponent: () => import('./features/componet/home/home.component').then((c) => c.HomeComponent)}
+    {path:"home", loadComponent: () => import('./features/pages/home/home.component').then((c) => c.HomeComponent)}
 
 
 ];
--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../../core/pages/navbar/navbar.component";
-import { PoularIteamsComponent } from "../../../core/pages/poular-iteams/poular-iteams.component";
-import {categoriesSliderComponent } from "../categories-slider/categories-slider.component";
+import { categoriesSliderComponent } from "../categories-slider/categories-slider.component";
+import { PoularIteamsComponent } from "../../componet/poular-iteams/poular-iteams.component";
+import { OurgallaryComponent } from "../../componet/ourgallary/ourgallary.component";
 
 
 @Component({
   selector: 'app-home',
-  imports: [PoularIteamsComponent, categoriesSliderComponent, categoriesSliderComponent],
+  imports: [categoriesSliderComponent, PoularIteamsComponent, OurgallaryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
