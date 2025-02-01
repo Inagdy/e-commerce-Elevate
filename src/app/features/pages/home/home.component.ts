8ac285172eaa88d221fc860db84f7725f9a69a4a
Inagdy <islam.m.nagdy@gmail.com>
2025-02-01 05:31:25 +0200

    updated

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { categoriesSliderComponent } from "../categories-slider/categories-slider.component";
 import { PoularIteamsComponent } from "../../componet/poular-iteams/poular-iteams.component";
 import { OurgallaryComponent } from "../../componet/ourgallary/ourgallary.component";
+import { AboutUsComponent } from "../trasted-by-us/trasted-by.component";
 
 
 @Component({
   selector: 'app-home',
-  imports: [categoriesSliderComponent, PoularIteamsComponent, OurgallaryComponent],
+  imports: [categoriesSliderComponent, PoularIteamsComponent, OurgallaryComponent, AboutUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
