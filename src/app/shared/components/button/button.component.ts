70c0f0403512872c62fca1787a5004b77e63a670
Inagdy <islam.m.nagdy@gmail.com>
2025-02-01 05:29:00 +0200

    choren(truested-by):create trasted-by re-usable component

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-botton',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class BottonComponent {
+
+
+}
