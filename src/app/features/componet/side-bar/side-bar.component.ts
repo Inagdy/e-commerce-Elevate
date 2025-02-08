a26f9321d4f64196ae97db1c199fa287642bbdab
Inagdy <islam.m.nagdy@gmail.com>
2025-02-08 06:45:04 +0200

    clore(slider-bar):adding card to the main component

--- src/app/features/componet/side-bar/side-bar.component.ts
+++ src/app/features/componet/side-bar/side-bar.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { SideBarCardComponent } from "../../../shared/components/side-bar-card/side-bar-card.component";
+import { FormsModule } from '@angular/forms';
+import { SideBarCardComponent } from '../../../shared/components/side-bar-card/side-bar-card.component';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [SideBarCardComponent],
+  imports: [SideBarCardComponent ,FormsModule],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.scss'
+  styleUrl: './side-bar.component.scss',
 })
 export class SideBarComponent {
-
+  public firstcard = [
+    'Home & Living',
+    'Garment Care',
+    'Jewelry & Accessories',
+    'Occasion Gifts',
+    'Office & Stationery',
+    'Personalised Gifts',
+    'Gifts Box',
+    'Other',
+  ];
+  secandcard =[
+    "Tovola",
+    "Sundoy",
+    "Sahoo Gifts",
+    "Casterly",
+    "Mainden Gifts"
+  ]
+  value!: number ; 
+  salescard = ["On Sale", "In Stock" ,"Out Of Stock","Discount"]
+  sizes = ["Extra Small","Small","Medium","Large","Extra Large"]
 }
