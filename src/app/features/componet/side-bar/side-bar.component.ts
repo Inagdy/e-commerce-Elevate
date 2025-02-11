0140e5cc197280f7825ec822a0edc92c1fe13a68
Inagdy <islam.m.nagdy@gmail.com>
2025-02-11 14:13:19 +0200

    last updated

--- src/app/features/componet/side-bar/side-bar.component.ts
+++ src/app/features/componet/side-bar/side-bar.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { SideBarCardComponent } from '../../../shared/components/side-bar-card/side-bar-card.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [SideBarCardComponent ,FormsModule],
+  imports: [SideBarCardComponent ,FormsModule ,NgClass,ReactiveFormsModule ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss',
 })
 export class SideBarComponent {
-  public firstcard = [
+
+  form = new FormGroup({
+    i: new FormControl(null),
+  })
+
+  firstcard = [
     'Home & Living',
     'Garment Care',
     'Jewelry & Accessories',
@@ -29,4 +35,19 @@ export class SideBarComponent {
   value!: number ; 
   salescard = ["On Sale", "In Stock" ,"Out Of Stock","Discount"]
   sizes = ["Extra Small","Small","Medium","Large","Extra Large"]
+  star = Array(5);
+  star_number:number = 4;
+  
+  public cards={
+    firstcard: this.firstcard,
+    secandcard: this.secandcard,
+    salescard: this.salescard,
+    sizes: this.sizes,
+    star: this.star,
+    star_number: this.star
+  }
+  getCheckedObjects() {
+    console.log(this.form.value); 
+
+  }
 }
--- src/app/shared/components/side-bar-card/side-bar-card.component.ts
+++ src/app/shared/components/side-bar-card/side-bar-card.component.ts
@@ -1,12 +1,15 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar-card',
-  imports: [],
+  imports: [ ],
   templateUrl: './side-bar-card.component.html',
   styleUrl: './side-bar-card.component.scss'
 })
 export class SideBarCardComponent {
  
 @Input() title!: string;  
+
+
 }
